baa50546b6cad46437960f441ea591343c5d36d3
joshuaevern <everncamed@gmail.com>
2020-09-23 02:42:52 +0200

    modified the sytling sheet

---
+++ cv.js
@@ -0,0 +1,22 @@
+window.addEventListener('load',() =>{
+    const params = (new URL(document.location)).searchParams;
+    const name = params.get('Firstname');
+    const middle = params.get('Middlename');
+    const last = params.get('Lastname');
+    const gender = params.get('Gender');
+    const email = params.get('email');
+    const DateOfBirth = params.get('DateOfBirth');
+    const address = params.get('Address');
+    const phone = params.get('phone');
+
+  
+
+    document.getElementById('Firstname').innerHTML = name;
+    document.getElementById('Middlename').innerHTML = middle;
+    document.getElementById('Lastname').innerHTML = last;
+    document.getElementById('Gender').innerHTML = gender;
+    document.getElementById('email').innerHTML = email;
+    document.getElementById('DateOfBirth').innerHTML = DateOfBirth;
+    document.getElementById('Address').innerHTML = address;
+    document.getElementById('phone').innerHTML = phone;
+})
